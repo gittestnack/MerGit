bc2987c4ebdfec058213de5638c2adc4302cf159
gittestnack <gittestnack@hotmail.se>
2012-11-08 08:29:31 +0100

    test

---
+++ mergit/src/test/MerGit.java
@@ -0,0 +1,14 @@
+package test;
+
+public class MerGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test");
+
+	}
+
+}
