3b084276c94c39e16de8be02b5f0eebd7e88340a
unknown <Annika.Lind@B00062.nackademin.local>
2012-11-08 08:47:40 +0100

    test

--- mergit/src/test/MerGit.java
+++ mergit/src/test/MerGit.java
@@ -7,7 +7,8 @@ public class MerGit {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Testar");
+		System.out.println("Testar igen");
+		//mer blaj
 
 	}
 
